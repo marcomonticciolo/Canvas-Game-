dfb0a52e1edb132545ad76ce8a3346659fbda646
marco monticciolo <marcomonticciolo@marcos-MBP.attlocal.net>
2022-11-01 16:14:46 -0400

    final commit before enemies

--- javascript.js
+++ javascript.js
@@ -1,34 +1,78 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-
+const timerDiv = document.querySelector('#timer')
 
 ctx.fillRect(50,400,200,50)
 
 const gravity = 1
 
+class Background {
+    constructor(x,y,sx,sy,swidth,sheight,width,height,imageSrc){
+        this.x = x;
+        this.y = y;
+        this.hitX = this.x + 50;
+        this.hitY = this.y + 30;
+        this.sx = sx;
+        this.sy = sy;
+        this.swidth = swidth;
+        this.sheight = sheight;
+        this.w = width
+        this.h = height
+        this.hitW = this.w - 100;
+        this.hitH = this.h - 50;
+        this.img = new Image()
+        this.img.src = imageSrc;
+
+    }
+
+    draw(){
+        
+        ctx.drawImage(this.img,this.sx,this.sy,this.swidth,this.sheight,this.x,this.y, this.w, this.h)
+     
+    }
+}
+
+class enemy {
+    constructor(x,y,width,height,imageSrc){
+        this.x = x
+        this.y = y
+        this.height = height
+        this.width = width
+        this.img = new Image()
+        this.img.src = imageSrc;
+    }
+
+    draw(){
+
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.x,this.y,this.w,this.h);
+    }
+}
+
+let backgroundImg = new Background(0,0,0,0,928,735,canvas.width, canvas.height,"./images/Free-Pixel-Art-Forest/Preview/Background.png")
+
+
+
+
 class Player {
     constructor({x,y,width,height,color,velocity}){
-        this.width = width;
-        this.height = height;
+        this.w = width;
+        this.h = height;
         this.color = color;
         this.velocity= velocity;
         this.x = x;
         this.y = y;
+        this.hasJumped = 0;
     }
 
     moveLeft(){
         this.velocity.x = -1
-        // if (this.velocity.x >= -5){
-        // //  this.velocity.x -= 4 / 40 * 100;}
     }
 
     moveRight(){
         this.velocity.x = 1
-        // if (this.velocity.x <= 5){
-        //     this.velocity.x += 4 / 40 * 100;
         }
-    // }
     stopX(){
         this.velocity.x = 0 / 40 * 100;
     }
@@ -36,18 +80,36 @@ class Player {
         this.velocity.y = 0 / 40 * 100;
     }
     jump(){
-        if (this.velocity.y  =-19);
-            // this.velocity.y -= 7 / 40 * 100;  
-        // }
-    
-
+        this.hasJumped++
+        console.log(this.hasJumped)
+        if(this.hasJumped < 3){
+            this.velocity.y  = -17; 
+        }
+        
     }
 
+    collisionDetection(object){
+        if (
+            this.x < object.hitX + object.hitW &&
+            this.x + this.w > object.hitX &&
+            this.y + this.velocity.y < object.hitY + object.hitH &&
+            this.h + this.y + this.velocity.y > object.hitY
+          ) {
+            if(this.velocity.y > 0 && this.y + this.h < object.hitY + object.hitY * 0.05){
+                this.velocity.y = 0;
+                this.y = object.hitY - this.h;
+                this.hasJumped = 0;
+            }
+            return true 
+          } else {
+            return false
+          }
+    }
     
     drawPlayer(){
         
         ctx.fillStyle = 'blue'
-        ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.fillRect(this.x,this.y,this.w,this.h);
     }
 
 update(){
@@ -57,13 +119,10 @@ update(){
     this.y += this.velocity.y
 
 // floor border
-    if (this.y + this.height + this.velocity.y >= canvas.height ){
+    if (this.y + this.h + this.velocity.y >= canvas.height ){
         this.velocity.y = 0
+        this.hasJumped = 0
      } else {this.velocity.y += gravity}
-   this.drawPlayer()
-
-//    if  (this.y + this.height + this.velocity.y >= )
-
 
 //    Ceiling border!
 if (this.y <= 0){
@@ -76,24 +135,14 @@ if (this.x <= 0){
     this.x= 0
 }
 // Right wall border
-if (this.x >= canvas.width - this.width){
+if (this.x >= canvas.width - this.w){
     this.velocity.x = -1
-    this.x = canvas.width - this.width - 1
+    this.x = canvas.width - this.w - 1
   
 }
-
-
-
-}
+this.drawPlayer()
 
 }
-
-class Islands extends Player {
-    drawIslands(){
-        ctx.fillStyle= 'black'
-        ctx.fillRect(this.x,this.y,this.width,this.height);
-
-    }
 }
 
 
@@ -111,32 +160,15 @@ let newPlayer = new Player({
 });
 
 
-let newIsland = new Islands({
-    x: 100,
-    y:420,
-    width:250,
-    height: 25,
-    color: 'black',
-   
-});
 
-let newIsland2 = new Islands({
-    x: 500,
-    y:300,
-    width:250,
-    height: 25,
-    color: 'black',
-   
-})
+// constructor(x,y,sx,sy,swidth,sheight,width,height,imageSrc)
 
-let newIsland3 = new Islands({
-    x: 75,
-    y:200,
-    width:250,
-    height: 25,
-    color: 'black',
-   
-})
+let newIsland = new Background(100,475,400,0,1200,470,300,100,"./images/Mossy-FloatingPlatforms.png")
+
+let newIsland2 = new Background(550,325,400,0,1200,470,300,100,"./images/Mossy-FloatingPlatforms.png")
+
+let newIsland3 = new Background(60,150,400,0,1200,470,300,100,"./images/Mossy-FloatingPlatforms.png")
+
 
 
 const keys = {
@@ -149,9 +181,6 @@ const keys = {
     Space: {
         pressed: false
     }
-    
-    
-
 }
 
 
@@ -187,20 +216,33 @@ window.addEventListener('keyup',function(event){
             break
         case "Space":
             newPlayer.stopX();
-            keys.Space.pressed = false
             break
     }
 })
 
+const islandsArr = [newIsland, newIsland2, newIsland3]
+
+let frameCount = 0;
+let score = 0;
+
 function animate(){
-    
+    // frameCount++
+    // if(frameCount % 60 === 0){
+    //    score++;
+    //    timerDiv.textContent = `time ${score}`
+    // }
     window.requestAnimationFrame(animate) 
     ctx.fillStyle = "white"
     ctx.fillRect (0,0,canvas.width,canvas.height)
-    newIsland.drawIslands() 
-    newIsland2.drawIslands()
-    newIsland3.drawIslands()
+    backgroundImg.draw()
     newPlayer.update()
+    for(let i = 0; i < islandsArr.length; i++){
+        if(newPlayer.collisionDetection(islandsArr[i])){
+            console.log('detected')
+        }
+        islandsArr[i].draw()
+    }
+   
 
 
     newPlayer.velocity.x = 0
@@ -219,33 +261,6 @@ function animate(){
     }
 
 animate()
-
-
-
-
-
-
-
-
-// let frames = 0
-
-// const animationLoop = () => {
-
-//     frames++
-
-
-// ctx.clearRect(0,0,canvas.width,canvas.height);
-
-
-// newPlayer.drawPlayer();
-
-
-// }
-
-// setInterval(animationLoop, 16)
-
-
-
 
 
 
